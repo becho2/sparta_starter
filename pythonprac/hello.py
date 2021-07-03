f907c28c7bba7c051ad9dcd448ffa2e965f74822
Jin <kimjh620@naver.com>
2021-07-03 20:12:31 +0900

    파이썬3-13강웹스크래핑 결과 몽고DB 저장까지 완료

---
+++ pythonprac/hello.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303', headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#old_content > table > tbody > tr:nth-child(2)
+
+trs = soup.select('#old_content > table > tbody > tr')
+
+for tr in trs:
+    a_tag = tr.select_one('a')
+    if a_tag != None:
+        title = a_tag.text
+        rank = tr.select_one('.ac > img')['alt']
+        point = tr.select_one('.point').text
+        doc = {'rank': rank, 'title': title, 'point': point}
+        db.movies.insert_one(doc)
